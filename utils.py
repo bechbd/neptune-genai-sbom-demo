ee607a41d0b5d55aec03e7999ea16ae1e79548f7
Dave Bechberger <dbechbe@amazon.com>
2024-06-18 08:30:24 -0700

    Updated with better replay of results

--- utils.py
+++ utils.py
@@ -13,6 +13,15 @@ def create_display(response, key=None):
     if isinstance(response, dict) or isinstance(response, list):
         if isinstance(response, dict) and "subgraph" in response:
             setup_graph(response["subgraph"], key=key)
+        elif isinstance(response, dict) and "results" in response:
+            if isinstance(response["results"], dict) or isinstance(
+                response["results"], list
+            ):
+                st.dataframe(response["results"], use_container_width=True)
+            elif "results" in response:
+                st.write(response["results"])
+            else:
+                st.write(response)
         else:
             st.dataframe(response, use_container_width=True)
 
